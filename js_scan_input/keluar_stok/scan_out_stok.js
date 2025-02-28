87e028b7668819751f4b045b56dedf460d90f719
root <joesandiroz2@gmail.com>
2025-02-28 16:52:42 +0700

    pengurangan stok

--- js_scan_input/keluar_stok/scan_out_stok.js
+++ js_scan_input/keluar_stok/scan_out_stok.js
@@ -35,15 +35,49 @@ export async function ScanOutStok(pb, input, dataItems) {
     const item = dataItems.find(record => record.Supplier_Part_Number === partNumber);
 
     if (item) {
-          // Ambil semua data kartu_stok berdasarkan part_number
-        const records = await pb.collection('kartu_stok').getFullList({
-            filter: `part_number="${partNumber}"`
-        });
-        // Hitung total balance dari semua record yang ditemukan
-        let totalBalance = records.reduce((sum, record) => sum + (parseInt(record.balance) || 0), 0);
 
-        // Hitung balance baru
-        let newBalance = totalBalance - parseInt(qty);
+        // Ambil semua data kartu_stok berdasarkan part_number dan no_dn
+        const records = await pb.collection('kartu_stok').getFullList({
+            filter: `part_number="${partNumber}" && no_dn="${item.Delivery_Note_No}"`
+        });
+
+        // Hitung total qty_ambil
+        const totalQtyAmbil = records.reduce((total, record) => total + parseInt(record.qty_ambil || 0), 0);
+
+        // Bandingkan total qty_ambil dengan item.Qty_DN
+        if (totalQtyAmbil + parseInt(qty) > item.Qty_DN) {
+            // Tampilkan SweetAlert jika kuantiti sudah penuh
+            Swal.fire({
+                title: 'Kuantiti sudah penuh!',
+                text: `Part number ${partNumber} sudah mencapai kuantiti maksimum.`,
+                icon: 'error',
+                timer: 2000,
+                showConfirmButton: false
+            });
+
+            // Memutar suara
+            const audio = new Audio('suara/kuantiti_penuh.mp3');
+            audio.play();
+
+            // Clear input field and set focus back
+            inputField.value = ''; // Clear the input field
+            inputField.focus(); // Set focus back to the input field
+            return; // Keluar dari fungsi jika kuantiti sudah penuh
+        }
+
+
+          // Ambil semua data kartu_stok berdasarkan part_number
+        const recordsbalance = await pb.collection('kartu_stok').getFullList({
+            filter: `part_number="${partNumber}"`,
+            sort: '-created' // Mengurutkan dari yang terbaru
+        });
+
+        // Ambil data terakhir (terbaru)
+        const lastRecord = recordsbalance.length > 0 ? recordsbalance[0] : null;
+        const lastBalance = lastRecord ? parseInt(lastRecord.balance) || 0 : 0;
+
+        // Hitung balance baru berdasarkan data terakhir
+        let newBalance = lastBalance - parseInt(qty);
 
 
         // Buat data untuk disimpan
