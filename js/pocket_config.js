668f1c69bdfadbb936e400a1cd308f526d3b95ce
root <joesandiroz2@gmail.com>
2025-02-28 14:49:40 +0700

    udah

---
+++ js/pocket_config.js
@@ -0,0 +1,10 @@
+let pocketbaseUrl = "http://127.0.0.1:8090"
+// let pocketbaseUrl = "https://capability-billy-inspector-gossip.trycloudflare.com"
+// let pocketbaseUrl = "https://strange-eel-40.telebit.io"
+let username_pocket = "daren.rasyidx@gmail.com"
+let user_pass_pocket = "admin12345"
+
+
+const admin_pocket = "bobwatcherx@gmail.com"; // Ganti dengan email admin
+const admin_pass_pocket = "amigos2010"; // Ganti dengan password admin
+
