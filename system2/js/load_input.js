6df394c51f06046855774d7af7c9b203f5e9b8e5
root <joesandiroz2@gmail.com>
2025-03-10 08:40:25 +0700

    fix mrgin print

--- system2/js/load_input.js
+++ system2/js/load_input.js
@@ -160,8 +160,8 @@ function openPrintModal(merk, partNumber, namaBarang, qty, satuan, lot, depo, su
 
 
             let labelHTML = `
-            <div  style="color:black;padding: 15px; width: calc(33.33% - 5px);margin:0px">
-                <div class=" label" style="border-radius:10px; border: 2px solid black;  padding: 5px; position: relative;">
+            <div  style="color:black;padding: 3px  55px; width: calc(33.33% - 5px);">
+                <div class=" label" style="border-radius:10px; border: 2px solid black;  padding: 2px; position: relative;">
                     <p style="font-size:10px;padding:0px;border-bottom:1px solid black;margin:0px;font-weight:bold;text-align:center; display:block;">PT. GRAFINDO MITRASEMESTA</p>
                      <p class="no-margin" style="font-size:10px;border-bottom: 1px solid black; display: block; width: 100%; margin: 1px 0; line-height: 1;">Part Name: <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${namaBarang}</b></p>
                             <p class="no-margin" style="font-size:10px;border-bottom: 1px solid black; display: block; width: 100%; margin: 2px 0; line-height: 1;">Part Number: <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${partNumber}</b></p>
@@ -175,7 +175,7 @@ function openPrintModal(merk, partNumber, namaBarang, qty, satuan, lot, depo, su
                         <p class="no-margin" style="font-size:10px;border-bottom: 1px solid black; display: block; width: 100%; margin: 2px 0; line-height: 1;">Opr Packing: <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${depo}</b></p>
                             
                         </div>
-                        <div style="text-align: right;margin-top:2px">
+                        <div style="text-align: right;margin-top:2px;margin-right:3px">
                             <div id="qrcode-${i}"></div>
                         </div>
                     </div>
