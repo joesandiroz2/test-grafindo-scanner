7c760e30607169fabcda0b9cc0bbe07677f0f9fe
joesandiroz2 <167519038+joesandiroz2@users.noreply.github.com>
2025-03-19 14:20:18 +0700

    Update load_input.js

--- system2_gse/js/load_input.js
+++ system2_gse/js/load_input.js
@@ -175,7 +175,7 @@ function openPrintModal(merk, partNumber, namaBarang, qty, satuan, berapa_lembar
 
 lembarHTML += `
 <div  style="color:black;padding: 3px  2px; width: calc(33.33% - 25px);display:inline-block">
-    <div class="label" style=" page-break-inside: avoid; width:320px;border-radius:10px; border: 1px solid black;position: relative;margin-bottom:25px;margin-left:50px">
+    <div class="label" style=" page-break-inside: avoid; width:320px;border-radius:10px; border: 1px solid black;position: relative;margin-bottom:25px;margin-left:45px">
         <p style="font-size:10px;padding:0px;border-bottom:1px solid black;margin:0px;text-align:center; display:block;">PT. GRAFINDO MITRASEMESTA</p>
          <p class="no-margin" style="font-size:10px;text-decoration:underline; display: block; width: 100%; margin: 1px 0; line-height: 1;">Part Name &nbsp;&nbsp;&nbsp;:&nbsp;${namaBarang}</p>
         <div style="line-height:0.5;display:flex;justify-content:space-between">
