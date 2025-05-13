a7edfa3cf470b72e89b6fc3e58f3f8a477037408
joesandiroz2 <167519038+joesandiroz2@users.noreply.github.com>
2025-05-13 13:11:49 +0700

    Update pocket_config.js

--- js/pocket_config.js
+++ js/pocket_config.js
@@ -1,5 +1,5 @@
 // let pocketbaseUrl = "http://127.0.0.1:8090"
-let pocketbaseUrl = "https://eclipse-redhead-hometown-victory.trycloudflare.com"
+let pocketbaseUrl = "https://aerospace-biology-silence-prefix.trycloudflare.com"
 // let pocketbaseUrl = "https://strange-eel-40.telebit.io"
 let username_pocket = "daren.rasyidx@gmail.com"
 let user_pass_pocket = "admin12345"
