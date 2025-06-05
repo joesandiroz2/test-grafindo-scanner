3763dc69cf7a2967dae6cd4393573bc3e60d66ec
joesandiroz2 <167519038+joesandiroz2@users.noreply.github.com>
2025-06-05 08:59:20 +0700

    Update pocket_config.js

--- js/pocket_config.js
+++ js/pocket_config.js
@@ -1,5 +1,5 @@
 // let pocketbaseUrl = "http://127.0.0.1:8090"
-let pocketbaseUrl = "https://rankings-arrives-kings-meaning.trycloudflare.com"
+let pocketbaseUrl = "https://nashville-pushing-joan-alliance.trycloudflare.com"
 let username_pocket = "daren.rasyidx@gmail.com"
 let user_pass_pocket = "admin12345"
 
