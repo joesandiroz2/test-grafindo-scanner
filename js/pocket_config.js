4034cbc59f122aa20a161fd9b35d1d24eb9c9c5b
joesandiroz2 <167519038+joesandiroz2@users.noreply.github.com>
2025-07-22 15:32:50 +0700

    Update pocket_config.js

--- js/pocket_config.js
+++ js/pocket_config.js
@@ -1,5 +1,5 @@
 // let pocketbaseUrl = "http://127.0.0.1:8090"
-let pocketbaseUrl = "https://clearance-chips-bo-syntax.trycloudflare.com"
+let pocketbaseUrl = "https://columns-milan-individuals-such.trycloudflare.com"
 let username_pocket = "daren.rasyidx@gmail.com"
 let user_pass_pocket = "admin12345"
 
