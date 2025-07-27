c44f8ace5a9ca82dea5bd8b94595e33d761a5e5f
joesandiroz2 <167519038+joesandiroz2@users.noreply.github.com>
2025-07-27 09:51:30 +0700

    Update pocket_config.js

--- js/pocket_config.js
+++ js/pocket_config.js
@@ -1,5 +1,5 @@
-let pocketbaseUrl = "http://127.0.0.1:8090"
-// let pocketbaseUrl = "https://lift-refer-lets-exclude.trycloudflare.com"
+//let pocketbaseUrl = "http://127.0.0.1:8090"
+ let pocketbaseUrl = "https://discounted-im-bone-recorded.trycloudflare.com"
 let username_pocket = "daren.rasyidx@gmail.com"
 let user_pass_pocket = "admin12345"
 
