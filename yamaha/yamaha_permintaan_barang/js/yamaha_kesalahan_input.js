5e1527bd500ac80ddd5dd333e61dfe20462b2a1f
root <joesandiroz2@gmail.com>
2025-08-01 14:22:26 +0700

    ubah minus qty kurangi

--- yamaha/yamaha_permintaan_barang/js/yamaha_kesalahan_input.js
+++ yamaha/yamaha_permintaan_barang/js/yamaha_kesalahan_input.js
@@ -35,7 +35,8 @@ async function Kurangi_stok_part() {
         if (latestItem) {
             balance = parseInt(latestItem.balance, 10) - parseInt(qty_kurangi); // Konversi ke integer sebelum menjumlahkan
         } else {
-            balance = qty_kurangi;
+            balance = -parseInt(qty_kurangi);
+
         }
 
         // Buat data untuk disimpan
